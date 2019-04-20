a2343e765446ec93a0b54b7ca0af0684e216e726
Allan Moura <allan.moura16@gmail.com>
2019-04-20 15:48:26 -0300

    updated Affiliated seeder to set addresses

--- database/seeds/AffiliatesSeeder.php
+++ database/seeds/AffiliatesSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Branch;
 use App\Affiliated;
+use App\Address;
 
 class AffiliatesSeeder extends Seeder
 {
@@ -19,17 +20,64 @@ class AffiliatesSeeder extends Seeder
         $branch3 = Branch::find(3);
 
         $affiliated1 = Affiliated::create(['name'=> 'fulano', 'cpf'=>'1234567890', 'sex'=>'Homem', 'email'=>'fulano@gmail.com', 'branch_id'=> $branch1->id]);
-        //$affiliated1->branch()->save($branch1);
+        $address1 = new Address;
+        $address1->cep = 53130330;
+        $address1->country = 'Brasil';
+        $address1->state = 'Pernambuco';
+        $address1->city = 'Olinda';
+        $address1->neighborhood = 'Casa caiada';
+        $address1->number = 140;
+        $address1->complement = 'Apt 201';
+        $address1->street = 'Rua Tertuliano Francisco feitosa';
+        $affiliated1->address()->save($address1);
+
         $affiliated2 = Affiliated::create(['name'=> 'ciclano', 'cpf'=>'1234567899', 'sex'=>'Homem', 'email'=>'ciclano@gmail.com', 'branch_id'=> $branch1->id]);
-        //$affiliated2->branch()->save($branch1);
+        $address1 = new Address;
+        $address1->cep = 53130331;
+        $address1->country = 'Brasil';
+        $address1->state = 'Pernambuco';
+        $address1->city = 'Recife';
+        $address1->neighborhood = 'Teste1';
+        $address1->number = 84;
+        $address1->complement = '';
+        $address1->street = 'Rua Chico';
+        $affiliated2->address()->save($address1);
+
         $affiliated3 = Affiliated::create(['name'=> 'beltrano', 'cpf'=>'1234567888', 'sex'=>'Homem', 'email'=>'beltrano@gmail.com', 'branch_id'=> $branch1->id]);
-        //$affiliated3->branch()->save($branch1);
+        $address1 = new Address;
+        $address1->cep = 52243022;
+        $address1->country = 'Brasil';
+        $address1->state = 'Pernambuco';
+        $address1->city = 'Igarrasu';
+        $address1->neighborhood = 'Cruz de rebolsa';
+        $address1->number = 32;
+        $address1->complement = 'preto da praça';
+        $address1->street = 'Rua qualquer rua';
+        $affiliated3->address()->save($address1);
+
         $affiliated4 = Affiliated::create(['name'=> 'frusteca', 'cpf'=>'1234567889', 'sex'=>'Mulher', 'email'=>'frusteca@gmail.com', 'branch_id'=> $branch2->id]);
-        //$affiliated1->branch()->save($branch2);
+        $address1 = new Address;
+        $address1->cep = 281858483;
+        $address1->country = 'Brasil';
+        $address1->state = 'Rio grande do norte';
+        $address1->city = 'Natal';
+        $address1->neighborhood = 'Parnamirim';
+        $address1->number = 14;
+        $address1->complement = '';
+        $address1->street = 'Rua de natal';
+        $affiliated4->address()->save($address1);
+
         $affiliated5 = Affiliated::create(['name'=> 'astolfo', 'cpf'=>'1234567222', 'sex'=>'Homem', 'email'=>'astolfo@gmail.com', 'branch_id'=> $branch2->id]);
-        //$affiliated1->branch()->save($branch2);
-        
-        
+        $address1 = new Address;
+        $address1->cep = 75938528;
+        $address1->country = 'States';
+        $address1->state = 'New York';
+        $address1->city = 'New York';
+        $address1->neighborhood = 'hells kitchen';
+        $address1->number = 4302;
+        $address1->complement = 'Qualquer complemente';
+        $address1->street = 'Rua da rua cheio de ruas';
+        $affiliated5->address()->save($address1);
         
     }
 }
