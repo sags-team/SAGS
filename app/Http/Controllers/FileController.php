aab2c2625f63f234ff7145cd128fde4f0d996432
Saulo Gomes <saulobr88@gmail.com>
2019-08-11 12:30:37 -0300

    Fixed some exceptions in file generation method

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -29,19 +29,18 @@ class FileController extends Controller
         $user = Auth::user();
         $bankInfo = BankInfo::where('branch_id', $user->branch->id)
         ->where('name', $request->input('name'))->first();
-        
+
         $affiliates = Affiliated::where('branch_id', $user->branch->id)->get();
         $content = "";
 
-        
         //Criando o Header do arquivo
         //A.01 - Código de registro = "A" - tamanho: 1 caractere;
         $content = $content."A";
         //A.02 - Código de Remessa - "1" - Remessa -> Enviado pela empresa para o banco "2" - Retorno Enviado pelo
         //Banco para a empresa - tamanho: 1 caractere
         $content = $content."1";
-        //A.03 Código do Convenio - Informado pelo Banco - 6 digitos do convenio 
-        //- tamanho: 20 caracteres(completar com brancos), 
+        //A.03 Código do Convenio - Informado pelo Banco - 6 digitos do convenio
+        //- tamanho: 20 caracteres(completar com brancos),
         $agreementCode = str_pad($bankInfo->agreementCode, 20, " ");
         $content = $content.$agreementCode;
         //A.04 Nome da empresa - tamanho: 20 caracteres(completar com brancos);
@@ -71,8 +70,11 @@ class FileController extends Controller
         $totalRegisters = 2;
         $totalValue = 0;
 
-        foreach($affiliates as $affiliated){
-            if($affiliated->bankAccount->active && $affiliated->bankAccount->name == $bankInfo->name){
+        foreach ($affiliates as $affiliated) {
+            if (!$affiliated->bankAccount) {
+                continue;
+            }
+            if ($affiliated->bankAccount->active && $affiliated->bankAccount->name == $bankInfo->name) {
                 //E.01 Código do registro = E
                 $EContent = "E";
                 //E.02 Identificação do cliente na empresa
@@ -114,11 +116,11 @@ class FileController extends Controller
         //Z.01 Código de registro = "Z";
         $content = $content."Z";
         //Z.02 Total de registros do arquivo
-        
+
         $totalRegisters = str_pad($totalRegisters, 6, "0", STR_PAD_LEFT);
         $content = $content.$totalRegisters;
         //Z.03 Valor total  dos campos E.06 e F.06 para arquivos de débito
-       
+
         $total = str_pad($totalValue, 17, "0", STR_PAD_LEFT);
         $content = $content.$total;
         //Z.04 Mesmo do Z.03 só que para Crédito
@@ -128,11 +130,11 @@ class FileController extends Controller
         //Z.05 Reservado para o futuro
         $filler = str_pad("", 109, " ");
         $content = $content.$filler;
-        
+
         Storage::disk('file')->put('COV'.date('m').'1'.'.TXT', $content);
         $bankInfo->fileCounter = $bankInfo->fileCounter + 1;
         $bankInfo->save();
-        $downloadFile = public_path().'\files\\'.'COV'.date('m').'1'.'.TXT';
+        $downloadFile = public_path().DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR.'COV'.date('m').'1'.'.TXT';
         return response()->download($downloadFile)->deleteFileAfterSend(true);
         //return Storage::download('file.txt');
         //Storage::delete('file.txt');
