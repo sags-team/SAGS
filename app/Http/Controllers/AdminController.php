35e9b1171624e8353e44eedbc7c4209436859ce8
Saulo Gomes <saulobr88@gmail.com>
2019-09-11 19:55:39 -0300

    Commented super and admin middlewares from __construct in Controllers

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('auth.admin');
+        //$this->middleware('auth.admin');
     }
 
     public function admin()
